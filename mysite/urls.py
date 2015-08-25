1e7e2d0bb130dcc046752db2e1d3c371c4161fe6
mariah <mariah.hamdi@gmail.com>
2015-08-24 19:43:02 -0700

    my blog, first commit

---
+++ mysite/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^myblog/', include('myblog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
